f9d1e118f39a4c5302ba7b5b87941b878390d9cc
Meitnerium9 <gronekmarcinn@gmail.com>
2026-05-17 13:53:19 +0200

    Koszyk (#5)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Koszyk from './koszyk/Koszyk.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -11,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/koszyk" element={<Koszyk />} />
         <Route path="/register" element={<Register />} />
         <Route path="/creator" element={<Creator />} />
       </Routes>
